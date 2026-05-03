f09356272bf630b3716e20035b56bc69ce30480f
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-05-03 22:37:51 +0530

    made chnages in code

--- Hello.java
+++ Hello.java
@@ -1,27 +1,27 @@
-class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
+// class Hello{
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
+// }
 
-class Hello{
-    public static void main(String args[]){
-        System.out.println(5+2);
-    }
-}
+// class Hello{
+//     public static void main(String args[]){
+//         System.out.println(5+2);
+//     }
+// }
 
-class Hello{
-    public static void main(String args[]){
-        int num=4;
-        System.out.println(num);
-    }
-}
+// class Hello{
+//     public static void main(String args[]){
+//         int num=4;
+//         System.out.println(num);
+//     }
+// }
 
-class Hello{
-    public static void main(String args[]){
-        int num1=18;
-        int num2=45;
-        int result=num1+num2;
-        System.out.println(result);
-    }
-}
+// class Hello{
+//     public static void main(String args[]){
+//         int num1=18;
+//         int num2=45;
+//         int result=num1+num2;
+//         System.out.println(result);
+//     }
+// }
