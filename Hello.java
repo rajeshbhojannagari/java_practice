5a9e0e5108c0275debd7fb08e185585b0d9394c5
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-05-03 21:52:18 +0530

    commented current code and coding another code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
+// class Hello{
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
+// }
