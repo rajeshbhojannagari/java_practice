aa61ca56741a40412c15c4b2592e140af0063d45
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-05-05 22:23:24 +0530

    change in code

--- Hello.java
+++ Hello.java
@@ -26,14 +26,14 @@
 //     }
 // }
 
-class Hello{
-    public static void main(String args[]){
-        int num1=12;
-        int num2=3;
-        int result=num1*num2;
-        System.out.println(result);
-    }
-}
+// class Hello{
+//     public static void main(String args[]){
+//         int num1=12;
+//         int num2=3;
+//         int result=num1*num2;
+//         System.out.println(result);
+//     }
+// }
 
 class Hello{
     public static void main(String args[]){
