43b4e863043e31a0ea8800b72bb7a4beb0c104d0
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-05-05 22:13:48 +0530

    coding

--- Hello.java
+++ Hello.java
@@ -47,5 +47,7 @@ class Hello{
     public static void main(String args[]){
         Scanner reader=new Scanner(System.in);
         System.out.println("Enter a number: ");
+        int number=reader.nextInt();
+        System.out.println(number);
     }
 }
