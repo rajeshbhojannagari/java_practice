c0d265b390d75ebf115f843409e7b836bff83f48
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-05-05 22:21:56 +0530

    change in code

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
-class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
+// class Hello{
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
+// }
 
 class Hello{
     public static void main(String args[]){
