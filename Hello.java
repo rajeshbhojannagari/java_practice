02c03359c1b2ee6625f0d9c8f79ff19b8396afde
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-15 22:24:08 +0530

    coding

--- Hello.java
+++ Hello.java
@@ -75,3 +75,5 @@ class Hello{
         int num = reader.nextInt();
         if(num % 2 == 0)
             System.out.println(num + " is even");
+         else
+            System.out.println(num + " is odd");
