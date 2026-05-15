faf698049f4d7b2b37e64a696453b8fb1501cd93
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-15 22:23:15 +0530

    coding

--- Hello.java
+++ Hello.java
@@ -72,3 +72,4 @@ class Hello{
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        int num = reader.nextInt();
