3d4224a639bed0e2642b3ef20bfa5faf810611c5
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-15 22:23:53 +0530

    coding

--- Hello.java
+++ Hello.java
@@ -73,3 +73,5 @@ class Hello{
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = reader.nextInt();
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
