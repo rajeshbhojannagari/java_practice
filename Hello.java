8e91f74e728afa5b26fe9e34e75bfa3f51ff95de
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-15 22:26:48 +0530

    commented code

--- Hello.java
+++ Hello.java
@@ -67,15 +67,15 @@
 //     }
 // }
 
-// import java.util.Scanner;
-// class Hello{
-//     public static void main(String[] args){
-//         Scanner reader = new Scanner(System.in);
-//         System.out.print("Enter a number: ");
-//         int num = reader.nextInt();
-//         if(num % 2 == 0)
-//             System.out.println(num + " is even");
-//          else
-//             System.out.println(num + " is odd");
-//          }
-// }
+import java.util.Scanner;
+class Hello{
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+         else
+            System.out.println(num + " is odd");
+         }
+}
